aebd7b653cd7ea8d2e24a65f25d9c03a5d1d15b2
Sanzid 2.0 <sanzid.official@gmail.comm>
2023-05-03 20:28:01 +0600

    server side first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 3000;
+
+const chef = require('./data/chef.json');
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Chef is cooking');
+})
+
+app.get('/chef', (req, res) => {
+    res.send(chef)
+})
+
+app.listen(port, () => {
+    console.log(`English professional chef is running on ${port}`);
+});
